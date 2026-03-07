ed7dd45bcfe75996187254dee37b50996bfc4b67
MidhunJohn24 <midhunjohn2408@gmail.com>
2026-03-07 12:21:19 +0530

    Add Android project and Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.getnotified.app',
+  appName: 'Get-Notified',
+  webDir: 'dist/get-notified'
+};
+
+export default config;
