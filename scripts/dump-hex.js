0412f182a427443b55b8ac6987a87b5687934aa9
MidhunJohn24 <midhunjohn2408@gmail.com>
2026-03-10 01:02:15 +0530

    chore: delete `verification_results.txt` and `debug_enrichment.txt`

--- scripts/dump-hex.js
+++
@@ -1,5 +0,0 @@
-const hex = 'CBMif0FVX3lxTE5wUXhXN2pWcXRUVlp6ZXZKWWVUMXZ1WUt6LVZleFdhbHRia0tHajh2UmV2R0NBMWh1cml0Q050bXlId19QajhVUG5zV0Vncnoyc3NkbjAwdnREUXhHZnNURzZ3NnV4S3NlZ3UwaThIcnJvVFFCR0s3YWQ4OHM3RjNnSFRfRWdERGdoeFE';
-const data = Buffer.from(hex, 'base64');
-console.log('Length:', data.length);
-console.log('Hex:', data.toString('hex'));
-console.log('Latin1:', data.toString('latin1'));
--- scripts/test-minimal.js
+++
@@ -1,7 +0,0 @@
-const https = require('https');
-
-https.get('https://news.google.com', (res) => {
-  console.log('Status:', res.statusCode);
-  res.on('data', () => {});
-  res.on('end', () => console.log('Done'));
-});
